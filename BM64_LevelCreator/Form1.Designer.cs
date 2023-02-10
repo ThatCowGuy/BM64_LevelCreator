d8b1ad6a0acf0411ed20c3f59d9c7d21ac5b413e
ThatCowGuy <cray40@gmx.de>
2023-02-10 16:01:45 +0100

    can now move viewports, can now edit map, started work on .bin export

--- BM64_LevelCreator/Form1.Designer.cs
+++ BM64_LevelCreator/Form1.Designer.cs
@@ -31,6 +31,9 @@ namespace BM64_LevelCreator
         {
             this.MapViewPanel = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.SectionViewPanel = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
@@ -38,47 +41,84 @@ namespace BM64_LevelCreator
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.LayerOverviewPanel = new System.Windows.Forms.Panel();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label5 = new System.Windows.Forms.Label();
+            this.LayerViewPanel = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.panel4.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // MapViewPanel
             // 
             this.MapViewPanel.BackColor = System.Drawing.Color.DimGray;
             this.MapViewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.MapViewPanel.Location = new System.Drawing.Point(3, 28);
+            this.MapViewPanel.Location = new System.Drawing.Point(4, 34);
+            this.MapViewPanel.Margin = new System.Windows.Forms.Padding(4);
             this.MapViewPanel.Name = "MapViewPanel";
-            this.MapViewPanel.Size = new System.Drawing.Size(261, 200);
+            this.MapViewPanel.Size = new System.Drawing.Size(347, 246);
             this.MapViewPanel.TabIndex = 0;
             this.MapViewPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MapViewPanel_Paint);
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.numericUpDown1);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.MapViewPanel);
-            this.panel2.Location = new System.Drawing.Point(383, 13);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(576, 8);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(349, 257);
+            this.panel2.Size = new System.Drawing.Size(459, 316);
             this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BM64_LevelCreator.Properties.Resources.DPad;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(366, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Layer";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(370, 67);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(9, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.Size = new System.Drawing.Size(114, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Full Map View";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -86,9 +126,10 @@ namespace BM64_LevelCreator
             // SectionViewPanel
             // 
             this.SectionViewPanel.BackColor = System.Drawing.Color.DimGray;
-            this.SectionViewPanel.Location = new System.Drawing.Point(3, 28);
+            this.SectionViewPanel.Location = new System.Drawing.Point(4, 34);
+            this.SectionViewPanel.Margin = new System.Windows.Forms.Padding(4);
             this.SectionViewPanel.Name = "SectionViewPanel";
-            this.SectionViewPanel.Size = new System.Drawing.Size(256, 256);
+            this.SectionViewPanel.Size = new System.Drawing.Size(341, 315);
             this.SectionViewPanel.TabIndex = 2;
             this.SectionViewPanel.Click += new System.EventHandler(this.SectionViewPanel_Click);
             this.SectionViewPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.SectionViewPanel_Paint);
@@ -100,17 +141,19 @@ namespace BM64_LevelCreator
             this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.label2);
             this.panel4.Controls.Add(this.SectionViewPanel);
-            this.panel4.Location = new System.Drawing.Point(12, 13);
+            this.panel4.Location = new System.Drawing.Point(13, 8);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(263, 480);
+            this.panel4.Size = new System.Drawing.Size(355, 591);
             this.panel4.TabIndex = 3;
             // 
             // TileInfoPanel
             // 
             this.TileInfoPanel.BackColor = System.Drawing.Color.DimGray;
-            this.TileInfoPanel.Location = new System.Drawing.Point(4, 317);
+            this.TileInfoPanel.Location = new System.Drawing.Point(5, 390);
+            this.TileInfoPanel.Margin = new System.Windows.Forms.Padding(4);
             this.TileInfoPanel.Name = "TileInfoPanel";
-            this.TileInfoPanel.Size = new System.Drawing.Size(256, 144);
+            this.TileInfoPanel.Size = new System.Drawing.Size(341, 177);
             this.TileInfoPanel.TabIndex = 3;
             this.TileInfoPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.TileInfoPanel_Paint);
             // 
@@ -119,10 +162,9 @@ namespace BM64_LevelCreator
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(11, 297);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(15, 366);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 17);
+            this.label4.Size = new System.Drawing.Size(129, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Current Tile Info";
             // 
@@ -131,89 +173,107 @@ namespace BM64_LevelCreator
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(11, 9);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(15, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 17);
+            this.label2.Size = new System.Drawing.Size(135, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Selected Section";
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.LayerOverviewPanel);
-            this.panel1.Location = new System.Drawing.Point(383, 246);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Controls.Add(this.LayerViewPanel);
+            this.panel1.Location = new System.Drawing.Point(576, 295);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(269, 257);
+            this.panel1.Size = new System.Drawing.Size(459, 316);
             this.panel1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BM64_LevelCreator.Properties.Resources.DPad;
+            this.pictureBox2.Location = new System.Drawing.Point(370, 106);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(67, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseClick);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(9, 9);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 11);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 17);
+            this.label3.Size = new System.Drawing.Size(121, 20);
             this.label3.TabIndex = 1;
             this.label3.Text = "Selected Layer";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // LayerOverviewPanel
+            // LayerViewPanel
             // 
-            this.LayerOverviewPanel.BackColor = System.Drawing.Color.DimGray;
-            this.LayerOverviewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LayerOverviewPanel.Location = new System.Drawing.Point(3, 28);
-            this.LayerOverviewPanel.Name = "LayerOverviewPanel";
-            this.LayerOverviewPanel.Size = new System.Drawing.Size(261, 200);
-            this.LayerOverviewPanel.TabIndex = 0;
-            this.LayerOverviewPanel.Scroll += new System.Windows.Forms.ScrollEventHandler(this.LayerOverviewPanel_Scroll);
-            this.LayerOverviewPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.LayerOverviewPanel_Paint);
-            this.LayerOverviewPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LayerOverviewPanel_MouseClick);
+            this.LayerViewPanel.BackColor = System.Drawing.Color.DimGray;
+            this.LayerViewPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LayerViewPanel.Location = new System.Drawing.Point(4, 34);
+            this.LayerViewPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.LayerViewPanel.Name = "LayerViewPanel";
+            this.LayerViewPanel.Size = new System.Drawing.Size(347, 246);
+            this.LayerViewPanel.TabIndex = 0;
+            this.LayerViewPanel.Scroll += new System.Windows.Forms.ScrollEventHandler(this.LayerOverviewPanel_Scroll);
+            this.LayerViewPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.LayerOverviewPanel_Paint);
+            this.LayerViewPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LayerOverviewPanel_MouseClick);
             // 
-            // numericUpDown1
+            // label6
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(270, 46);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
-            this.numericUpDown1.TabIndex = 2;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(366, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Section";
             // 
-            // label5
+            // textBox1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label5.Location = new System.Drawing.Point(269, 26);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 17);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Layer";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(370, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "( 0 | 0 )";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(800, 505);
+            this.ClientSize = new System.Drawing.Size(1088, 622);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Bomberman 64 - Level Editor";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -228,11 +288,15 @@ namespace BM64_LevelCreator
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Panel LayerOverviewPanel;
+        private System.Windows.Forms.Panel LayerViewPanel;
         private System.Windows.Forms.Panel TileInfoPanel;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
